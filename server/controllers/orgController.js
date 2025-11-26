c3029d1008e091e1dda5ec3f46ef02aae3456ce8
BillyBourbon <ballinson18@gmail.com>
2025-11-26 19:07:39 +0000

    fdhdfghdfh

--- server/controllers/orgController.js
+++ server/controllers/orgController.js
@@ -3,7 +3,7 @@ const db = require("../config/db");
 // GET STAFF'S ORGANISATION INFO
 const getStaffOrganisation = (user_id, callback) => {
   const staffOrgQuery = `
-    SELECT O.org_id, O.name 
+    SELECT O.org_id, O.name, OS.org_staff_id
     FROM ORGANISATION O
     JOIN ORGANISATION_STAFF OS ON O.org_id = OS.org_id
     WHERE OS.user_id = ? AND OS.is_active = 1
@@ -28,7 +28,7 @@ const getActiveOrganisations = (req, res) => {
     if (err) {
       return res.status(500).json({
         errMessage: "Database error while retrieving organisations",
-        error: err.message
+        error: err.message,
       });
     }
 
@@ -42,6 +42,7 @@ const getAllDonationRequests = (req, res) => {
 
   const donationRequestsQuery = `
   SELECT 
+    transaction_id,
     item_name, 
     category, 
     item_condition, 
@@ -49,7 +50,10 @@ const getAllDonationRequests = (req, res) => {
     photo_url, 
     status, 
     submitted_at, 
-    description
+    description,
+    handled_by_staff_id,
+    handled_at,
+    reason
   FROM DONATION_TRANSACTION
   WHERE org_id = ?`;
 
@@ -57,16 +61,69 @@ const getAllDonationRequests = (req, res) => {
     if (err) {
       return res.status(500).json({
         errMessage: "Database error while retrieving donation requests",
-        error: err.message
+        error: err.message,
       });
     }
 
-    return res.status(200).json(rows);
+    // used to check if photo_url is already a full URL
+    const isURL = (value) => {
+      try {
+        new URL(value);
+        return true;
+      } catch {
+        return false;
+      }
+    };
+    // modify photo_url to be full URL if not already
+    const modifiedRows = rows.map((row) => {
+      if (row.photo_url && !isURL(row.photo_url)) {
+        return {
+          ...row,
+          photo_url: `${process.env.BACKEND_URL}${row.photo_url}`,
+        };
+      } else {
+        return row;
+      }
+    });
+    return res.status(200).json(modifiedRows);
   });
-}
+};
+
+const updateDontationRequestStatus = (req, res) => {
+  console.log("Received request to update donation request status");
+  console.log("Request params:", req.params);
+  console.log("Request body:", req.body);
+
+  const { transaction_id } = req.params;
+  const { status, handled_by_staff_id, reason } = req.body;
+
+  const updateStatusQuery = `
+    UPDATE DONATION_TRANSACTION
+    SET status = ?, handled_by_staff_id = ?, handled_at = CURRENT_TIMESTAMP, reason = ?
+    WHERE transaction_id = ?`;
+
+  db.run(
+    updateStatusQuery,
+    [status, handled_by_staff_id, reason, transaction_id],
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          errMessage: "Database error while updating donation request status",
+          error: err.message,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Donation request status updated successfully",
+        changes: this.changes,
+      });
+    }
+  );
+};
 
 module.exports = {
   getStaffOrganisation,
   getActiveOrganisations,
   getAllDonationRequests,
+  updateDontationRequestStatus,
 };
